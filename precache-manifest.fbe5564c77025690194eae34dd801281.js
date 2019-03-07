af4d0c5c942febffcc30b843d06d9b3d271ee51c
Emily Shoji <eshoji1@lion.lmu.edu>
2019-03-07 00:48:20 -0800

    Updates

---
+++ precache-manifest.fbe5564c77025690194eae34dd801281.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "507991182da2a63f6f8f",
+    "url": "/static/css/main.19a2a17a.chunk.css"
+  },
+  {
+    "revision": "507991182da2a63f6f8f",
+    "url": "/static/js/main.d5b93bf5.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "df40e54214f5a0939fc0",
+    "url": "/static/js/2.85a899fe.chunk.js"
+  },
+  {
+    "revision": "6a607cbe397bb28afea6deb4870c17ee",
+    "url": "/index.html"
+  }
+];
