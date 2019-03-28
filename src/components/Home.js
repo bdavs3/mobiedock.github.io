b8ea4ab8053951acf66cece40f4d532cd9af6ddd
danicobryan <danicobryan@gmail.com>
2019-03-27 22:51:41 -0700

    updated landing page photo

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import { Jumbotron, Row, Col, Image, Button } from 'react-bootstrap';
 import './Home.css';
 
-const img = require('../dock2.jpg');
+const img = require('../shed-lmu2.png');
 const fullPageStyle = {
   height: '100vh',
   backgroundImage: `url(${img})`,
