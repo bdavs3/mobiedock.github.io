c5842f4cbf27d6ce2422aa10e2e7dd9861c74e15
danicobryan <danicobryan@gmail.com>
2019-04-04 14:17:38 -0700

    Fixed everything lmao

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,19 @@
 import React from "react";
 import { MDBContainer, MDBFooter } from "mdbreact";
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import 'bootstrap-css-only/css/bootstrap.min.css';
-import 'mdbreact/dist/css/mdb.css';
+import { Row, Col, Image, Button, Container, Carousel } from 'react-bootstrap';
+//import 'bootstrap-css-only/css/bootstrap.min.css';
+//import 'mdbreact/dist/css/mdb.css';
 import logo from "../images/mobiedock.png";
 
 import './Footer.css'
 
 const FooterPage = () => {
   return (
-    <MDBFooter color="black" className="center font-small pt-4 mt-4">
-        <MDBContainer fluid>
+    <div className="footer bg-black py-3">
+      <Container>
         <a href="#home"> <img src={logo} alt="logo"/></a> &copy; {new Date().getFullYear()}
-        </MDBContainer>
-    </MDBFooter>
+      </Container>
+    </div>
   );
 }
 
