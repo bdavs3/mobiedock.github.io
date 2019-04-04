322505e5f58520a5edc4d6a16e5eeb8d02313462
danicobryan <danicobryan@gmail.com>
2019-04-03 23:02:37 -0700

    added animations

--- src/components/Solution.js
+++ src/components/Solution.js
@@ -3,16 +3,23 @@ import { Link } from 'react-router-dom';
 import { Row, Col, Image, Button, Container } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './Solution.css';
+import WOW from "wowjs";
 
 
 export default class Solution extends Component {
+
+  componentDidMount() {
+    const wow = new WOW.WOW();
+    wow.init();
+  }
+
   render() {
     return (
       <div>
         <section id="about" className="py-5">
           <div className=" container text-center my-5">
             <h1> Our Solution </h1>
-            <h6 className="my-4">
+            <h6 className="my-4 mx-5">
             MobieDock is the first sustainably powered eDocking and charging solution for the shared micromobility market.
             Sleek stations declutter your city or campus, charge devices all day and night, and monetize this growing market –
             all while harnessing the sun’s energy for zero emissions.
@@ -24,13 +31,16 @@ export default class Solution extends Component {
       		<Container>
       		  <Row>
 
-              <Col lg={6} sm={6} className="text-left">
+              <Col lg={6} sm={6} className="text-left wow fadeInUp" data-wow-delay="0.5s">
                 <Image src={require('../images/dock-solo-1.jpg')} fluid roundedCircle/>
               </Col>
 
               <Col lg={6} sm={6}>
+                <Container>
+                  <h1 className="py-3 wow fadeInUp" data-wow-delay="0.5s"> Charging Stations </h1>
+                </Container>
 
-      					<div className="service-list">
+      					<div className="service-list wow fadeInRight" data-wow-delay="0.5s">
       						<div className="service-list-col1">
       							<FontAwesomeIcon className="icon" icon="solar-panel" />
       						</div>
@@ -39,7 +49,7 @@ export default class Solution extends Component {
       							<p>Use leading-edge solar technology for a zero net energy charging solution.</p>
       						</div>
       					</div>
-      					<div className="service-list">
+      					<div className="service-list wow fadeInRight" data-wow-delay="0.5s">
       						<div className="service-list-col1">
       							<FontAwesomeIcon className="icon" icon="sitemap" />
       						</div>
@@ -48,7 +58,7 @@ export default class Solution extends Component {
       							<p>Dock shared MDs for increased organization and safety.</p>
       						</div>
       					</div>
-      					<div className="service-list">
+      					<div className="service-list wow fadeInRight" data-wow-delay="0.5s">
       						<div className="service-list-col1">
       							<FontAwesomeIcon className="icon" icon="money-check-alt" />
       						</div>
@@ -57,6 +67,54 @@ export default class Solution extends Component {
       							<p>Monetize charging and traffic to benefit all parties.</p>
       						</div>
       					</div>
+              </Col>
+
+
+      			</Row>
+      		</Container>
+      	</section>
+
+        <section className="main-section py-5">
+      		<Container className = "py-5">
+      		  <Row>
+
+              <Col lg={6} sm={6}>
+
+                <Container>
+                  <h1 className="py-3 wow fadeInUp" data-wow-delay="0.5s"> App Integration </h1>
+                </Container>
+
+      					<div className="service-list wow fadeInLeft" data-wow-delay="0.5s">
+      						<div className="service-list-col1">
+      							<FontAwesomeIcon className="icon" icon="map-marked-alt" />
+      						</div>
+      						<div className="service-list-col2">
+      							<h3>Find a Ride or Station</h3>
+      							<p>Find nearby electric scooters or a place to charge.</p>
+      						</div>
+      					</div>
+      					<div className="service-list wow fadeInLeft" data-wow-delay="0.5s">
+      						<div className="service-list-col1">
+      							<FontAwesomeIcon className="icon" icon="sitemap" />
+      						</div>
+      						<div className="service-list-col2">
+      							<h3>Organize</h3>
+      							<p>Dock shared MDs for increased organization and safety.</p>
+      						</div>
+      					</div>
+      					<div className="service-list wow fadeInLeft" data-wow-delay="0.5s">
+      						<div className="service-list-col1">
+      							<FontAwesomeIcon className="icon" icon="money-check-alt" />
+      						</div>
+      						<div className="service-list-col2">
+      							<h3>Monetize</h3>
+      							<p>Monetize charging and traffic to benefit all parties.</p>
+      						</div>
+      					</div>
+              </Col>
+
+              <Col lg={6} sm={6} className="text-center wow fadeInUp" data-wow-delay="0.5s">
+                <Image src={require('../images/homepage-1.png')} fluid/>
               </Col>
 
 
