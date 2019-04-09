4c33d1b180f4d4dff90261f9e259e807fac3679b
Ben Davis <bdavs3000@gmail.com>
2019-04-09 00:21:21 -0700

    modified footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,13 @@
 import React from "react";
-import { MDBContainer, MDBFooter } from "mdbreact";
-import { Row, Col, Image, Button, Container, Carousel } from 'react-bootstrap';
-//import 'bootstrap-css-only/css/bootstrap.min.css';
-//import 'mdbreact/dist/css/mdb.css';
 import logo from "../images/mobiedock.png";
+import react_logo from "../images/react-logo.png";
 
-import './Footer.css'
+import './Footer.css';
 
 const FooterPage = () => {
   return (
     <div className="footer bg-black py-3">
-        <a href="#home"> <img src={logo} alt="logo"/></a> 
-        <p className="powered">Powered by:</p>
+        <a href="#home"> <img src={logo} alt="logo"/></a>
     </div>
   );
 }
