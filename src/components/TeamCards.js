e5018d395a1fb3ff9c55f9eccf86542c584b17b2
Emily Shoji <eshoji1@lion.lmu.edu>
2019-04-08 23:01:14 -0700

    updated team cards

--- src/components/TeamCards.js
+++ src/components/TeamCards.js
@@ -11,7 +11,7 @@ import billy from '../images/team-imgs/billy.jpeg';
 import charity from '../images/team-imgs/charity.jpeg';
 import masaki from '../images/team-imgs/masaki.jpeg';
 import matt from '../images/team-imgs/matt.jpeg';
-import alex from '../images/team-imgs/alex.jpeg';
+import alex from '../images/team-imgs/alex.png';
 import ahmad from '../images/team-imgs/ahmad.jpeg';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -29,19 +29,18 @@ class CardExample extends Component {
 	render() {
 		return (
 			<Container className="cards">
-				<CardDeck className="lead wow fadeInUp" data-wow-delay={CARD_DELAY}>
+				<CardDeck className="deck lead wow fadeInUp" data-wow-delay={CARD_DELAY}>
 					<Card>
 						<CardImg className="photo" top width="100%" src={billy} alt="Billy Walker" />
 						<CardBody>
 							<CardTitle className="cardTitle">Billy Walker</CardTitle>
 							<CardSubtitle className="subtitle">CEO / Co-founder</CardSubtitle>
 							<CardText className="text">
-								This is a wider card with supporting text below as a natural lead-in to additional
-								content. This content is a little bit longer.
+							Billy combines his innovative engineering background with a customer focused business sense to develop high value ventures. Billy's sales experience– both B2B and ecommerce– has summed to six digits in sales as a college student. An extensive tech, healthcare, and education network in Los Angeles, Silicon Valley, and Minneapolis gives Billy and MobieDock the avenues for success.
 							</CardText>
 						</CardBody>
 						<CardFooter>
-							<a href="http://google.com">
+							<a href="https://www.linkedin.com/in/billy-walker-4aa914111/">
 								<FontAwesomeIcon ClassName="icon" icon={[ 'fab', 'linkedin' ]} />
 							</a>
 						</CardFooter>
@@ -80,11 +79,11 @@ class CardExample extends Component {
 						</CardFooter>
 					</Card>
 					<Card>
-						<CardImg top width="100%" src={ben} alt="Ben Davis" />
+						<CardImg className="photo" top width="100%" src={ben} alt="Ben Davis" />
 						<CardBody>
-							<CardTitle>Ben Davis</CardTitle>
-							<CardSubtitle>Head of Software Systems</CardSubtitle>
-							<CardText>
+							<CardTitle className="cardTitle">Ben Davis</CardTitle>
+							<CardSubtitle className="subtitle">Head of Software Systems</CardSubtitle>
+							<CardText className="text">
 								Ben is involved with app development and software system integration.  Aside from computer science, Ben takes interest in electronic music, running, and physics.  Mobiedock is an intersection of many of the fields within computer science that he is particularly excited about.
 							</CardText>
 						</CardBody>
@@ -95,13 +94,10 @@ class CardExample extends Component {
 							<a href="https://github.com/bdavs3">
 								<FontAwesomeIcon ClassName="icon" icon={[ 'fab', 'github' ]} />
 							</a>
-							<a href="http://google.com">
-								<FontAwesomeIcon ClassName="icon" icon={[ 'fas', 'user-circle' ]} />
-							</a>
 						</CardFooter>
 					</Card>
 					<Card>
-						<CardImg top width="100%" src={matt} alt="Matthew Tejada" />
+						<CardImg className="photo" top width="100%" src={matt} alt="Matthew Tejada" />
 						<CardBody>
 							<CardTitle className="cardTitle">Matthew Tejada</CardTitle>
 							<CardSubtitle className="subtitle">CFO</CardSubtitle>
@@ -142,12 +138,12 @@ class CardExample extends Component {
 						</CardFooter>
 					</Card>
 					<Card>
-						<CardImg top width="100%" src={alex} alt="Alex Lee" />
+						<CardImg className="photo" top width="100%" src={alex} alt="Alex Lee" />
 						<CardBody>
-							<CardTitle>Alex Lee</CardTitle>
-							<CardSubtitle>Electrical Engineering</CardSubtitle>
+							<CardTitle className="cardTitle">Alex Lee</CardTitle>
+							<CardSubtitle className="subtitle">Electrical Engineering</CardSubtitle>
 							<CardText>
-								Alex is both a talented electrical engineer and athlete.  At Mobiedock, he is working on electrical hardware components for the stations.  He is also on a team at Loyola Marymount that is implementing an innovative space technology within the Earth-Moon-Earth communication project.  When he's not tinkering with circuit boards and modeling software, Alex competes at a national level in kayaking with Team USA and is training for the 2020 Olympics.  
+								Alex is both a talented electrical engineer and athlete. At Mobiedock, he is working on electrical hardware components for the stations.  He is also on a team at Loyola Marymount that is implementing an innovative space technology within the Earth-Moon-Earth communication project.  When he's not tinkering with circuit boards and modeling software, Alex competes at a national level in kayaking with Team USA and is training for the 2020 Olympics.  
 							</CardText>
 						</CardBody>
 						<CardFooter>
@@ -157,10 +153,10 @@ class CardExample extends Component {
 						</CardFooter>
 					</Card>
 					<Card>
-						<CardImg top width="100%" src={andre} alt="Andre De Leon" />
+						<CardImg className="photo" top width="100%" src={andre} alt="Andre De Leon" />
 						<CardBody>
-							<CardTitle>Andre De Leon</CardTitle>
-							<CardSubtitle>Mechanical Engineering</CardSubtitle>
+							<CardTitle className="cardTitle">Andre De Leon</CardTitle>
+							<CardSubtitle className="subtitle">Mechanical Engineering</CardSubtitle>
 							<CardText>
 								This is a wider card with supporting text below as a natural lead-in to additional
 								content. This content is a little bit longer.
@@ -174,15 +170,14 @@ class CardExample extends Component {
 					</Card>
 				</CardDeck>
 				
-				<CardDeck className="lead wow fadeInUp" data-wow-delay={CARD_DELAY}>
+				<CardDeck className="deck2 lead wow fadeInUp mt-2" data-wow-delay={CARD_DELAY}>
 					<Card>
-						<CardImg top width="100%" src={emily} alt="Emily Shoji" />
+						<CardImg className="photo" top width="100%" src={emily} alt="Emily Shoji" />
 						<CardBody>
-							<CardTitle>Emily Shoji</CardTitle>
-							<CardSubtitle>Frontend Developer</CardSubtitle>
+							<CardTitle className="cardTitle">Emily Shoji</CardTitle>
+							<CardSubtitle className="subtitle">Frontend Developer</CardSubtitle>
 							<CardText>
-								This is a wider card with supporting text below as a natural lead-in to additional
-								content. This content is a little bit longer.
+								A Computer Science major & Pure Mathematics Minor at LMU. Emily is origiinally from Seattle, WA and enjoys teaching and being outdoors.
 							</CardText>
 						</CardBody>
 						<CardFooter>
@@ -198,12 +193,12 @@ class CardExample extends Component {
 						</CardFooter>
 					</Card>
 					<Card>
-						<CardImg top width="100%" src={nico} alt="Danico Pidlaoan" />
+						<CardImg className="photo" top width="100%" src={nico} alt="Danico Pidlaoan" />
 						<CardBody>
-							<CardTitle>Danico Pidlaoan</CardTitle>
-							<CardSubtitle>Project Manager / Frontend Developer</CardSubtitle>
+							<CardTitle className="cardTitle">Danico Pidlaoan</CardTitle>
+							<CardSubtitle className="subtitle">Project Manager / Frontend Developer</CardSubtitle>
 							<CardText>
-								This card has supporting text below as a natural lead-in to additional content.
+								An asipring Software Developer and Project Manager at LMU, Nico is a Senior Computer Science Major and Business Minor.
 							</CardText>
 						</CardBody>
 						<CardFooter>
