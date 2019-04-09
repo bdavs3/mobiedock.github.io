8648eb6e30e355c29f94705559db4f5ddcc7813b
Ben Davis <bdavs3000@gmail.com>
2019-04-08 21:38:27 -0700

    Resolved merge conflicts

--- src/components/TeamCards.js
+++ src/components/TeamCards.js
@@ -11,12 +11,14 @@ import billy from '../images/team-imgs/billy.jpeg';
 import charity from '../images/team-imgs/charity.jpeg';
 import masaki from '../images/team-imgs/masaki.jpeg';
 import matt from '../images/team-imgs/matt.jpeg';
-import alex from '../images/team-imgs/profile.png';
+import alex from '../images/team-imgs/alex.jpeg';
 import ahmad from '../images/team-imgs/ahmad.jpeg';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import WOW from 'wowjs';
+
+const CARD_DELAY = "0.25s";
 
 class CardExample extends Component {
 	componentDidMount() {
@@ -27,12 +29,12 @@ class CardExample extends Component {
 	render() {
 		return (
 			<Container className="cards">
-				<CardDeck className="lead wow fadeInUp" data-wow-delay="0.5s">
+				<CardDeck className="lead wow fadeInUp" data-wow-delay={CARD_DELAY}>
 					<Card>
 						<CardImg className="photo" top width="100%" src={billy} alt="Billy Walker" />
 						<CardBody>
 							<CardTitle className="cardTitle">Billy Walker</CardTitle>
-							<CardSubtitle className="subtitle">Card subtitle</CardSubtitle>
+							<CardSubtitle className="subtitle">CEO / Co-founder</CardSubtitle>
 							<CardText className="text">
 								This is a wider card with supporting text below as a natural lead-in to additional
 								content. This content is a little bit longer.
@@ -48,7 +50,7 @@ class CardExample extends Component {
 						<CardImg className="photo" top width="100%" src={masaki} alt="Masaki Takamatsu" />
 						<CardBody>
 							<CardTitle className="cardTitle">Masaki Takamatsu</CardTitle>
-							<CardSubtitle className="subtitle">Card subtitle</CardSubtitle>
+							<CardSubtitle className="subtitle">CTO / Co-founder</CardSubtitle>
 							<CardText className="text">
 								This card has supporting text below as a natural lead-in to additional content.
 							</CardText>
@@ -59,6 +61,9 @@ class CardExample extends Component {
 							</a>
 						</CardFooter>
 					</Card>
+				</CardDeck>
+
+				<CardDeck className="lead wow fadeInUp" data-wow-delay={CARD_DELAY}>
 					<Card>
 						<CardImg className="photo" top width="100%" src={charity} alt="Charity Waddy" />
 						<CardBody>
@@ -74,108 +79,12 @@ class CardExample extends Component {
 							</a>
 						</CardFooter>
 					</Card>
-				</CardDeck>
-
-				<CardDeck className="lead wow fadeInUp" data-wow-delay="0.5s">
 					<Card>
-						<CardImg className="photo" top width="100%" src={andre} alt="Andre De Leon" />
+						<CardImg top width="100%" src={ben} alt="Ben Davis" />
 						<CardBody>
-							<CardTitle className="cardTitle">Andre De Leon</CardTitle>
-							<CardSubtitle className="subtitle">Card subtitle</CardSubtitle>
-							<CardText className="text">
-								This is a wider card with supporting text below as a natural lead-in to additional
-								content. This content is a little bit longer.
-							</CardText>
-						</CardBody>
-						<CardFooter>
-							<a href="http://google.com">
-								<FontAwesomeIcon ClassName="icon" icon={[ 'fab', 'linkedin' ]} />
-							</a>
-						</CardFooter>
-					</Card>
-					<Card>
-						<CardImg className="photo" top width="100%" src={ahmad} alt="Ahmad Kalifeh" />
-						<CardBody>
-							<CardTitle className="cardTitle">Ahmad Kalifeh</CardTitle>
-							<CardSubtitle className="subtitle">Card subtitle</CardSubtitle>
-							<CardText className="text">
-								This card has supporting text below as a natural lead-in to additional content.
-							</CardText>
-						</CardBody>
-						<CardFooter>
-							<a href="http://google.com">
-								<FontAwesomeIcon ClassName="icon" icon={[ 'fab', 'linkedin' ]} />
-							</a>
-						</CardFooter>
-					</Card>
-					<Card>
-						<CardImg className="photo" top width="100%" src={matt} alt="Matthew Tejada" />
-						<CardBody>
-							<CardTitle className="cardTitle">Matthew Tejada</CardTitle>
-							<CardSubtitle className="subtitle">Card subtitle</CardSubtitle>
-							<CardText className="text">
-								This is a wider card with supporting text below as a natural lead-in to additional
-								content. This card has even longer content than the first to show that equal height
-								action.
-							</CardText>
-						</CardBody>
-						<CardFooter>
-							<a href="http://google.com">
-								<FontAwesomeIcon ClassName="icon" icon={[ 'fab', 'linkedin' ]} />
-							</a>
-						</CardFooter>
-					</Card>
-				</CardDeck>
-
-				<CardDeck className="lead wow fadeInUp" data-wow-delay="0.5s">
-					<Card>
-						<CardImg className="photo" top width="100%" src={emily} alt="Emily Shoji" />
-						<CardBody>
-							<CardTitle className="cardTitle">Emily Shoji</CardTitle>
-							<CardSubtitle className="subtitle">Software Developer</CardSubtitle>
-							<CardText className="text">
-								A Computer Science Major & Pure Mathematics minor from Seattle, WA who enjoys front-end development.
-							</CardText>
-						</CardBody>
-						<CardFooter>
-							<a href="https://www.linkedin.com/in/emily-shoji/">
-								<FontAwesomeIcon ClassName="icon" icon={[ 'fab', 'linkedin' ]} />
-							</a>
-							<a href="https://github.com/ItsEmShoji">
-								<FontAwesomeIcon ClassName="icon" icon={[ 'fab', 'github' ]} />
-							</a>
-							<a href="https://itsemshoji.github.io/">
-								<FontAwesomeIcon ClassName="icon" icon={[ 'fas', 'user-circle' ]} />
-							</a>
-						</CardFooter>
-					</Card>
-					<Card>
-						<CardImg className="photo" top width="100%" src={nico} alt="Danico Pidlaoan" />
-						<CardBody>
-							<CardTitle className="cardTitle">Danico Pidlaoan</CardTitle>
-							<CardSubtitle className="subtitle">Software Developer</CardSubtitle>
-							<CardText className="text">
-								This card has supporting text below as a natural lead-in to additional content.
-							</CardText>
-						</CardBody>
-						<CardFooter>
-							<a href="https://www.linkedin.com/in/danico-pidlaoan/">
-								<FontAwesomeIcon ClassName="icon" icon={[ 'fab', 'linkedin' ]} />
-							</a>
-							<a href="https://github.com/danicobryan">
-								<FontAwesomeIcon ClassName="icon" icon={[ 'fab', 'github' ]} />
-							</a>
-							<a href="https://danicobryan.github.io/">
-								<FontAwesomeIcon ClassName="icon" icon={[ 'fas', 'user-circle' ]} />
-							</a>
-						</CardFooter>
-					</Card>
-					<Card>
-						<CardImg className="photo" top width="100%" src={ben} alt="Ben Davis" />
-						<CardBody>
-							<CardTitle className="cardTitle">Ben Davis</CardTitle>
-							<CardSubtitle className="subtitle">Software Developer</CardSubtitle>
-							<CardText className="text">
+							<CardTitle>Ben Davis</CardTitle>
+							<CardSubtitle>Head of Software Systems</CardSubtitle>
+							<CardText>
 								Ben is involved with app development and software system integration.  Aside from computer science, Ben takes interest in electronic music, running, and physics.  Mobiedock is an intersection of many of the fields within computer science that he is particularly excited about.
 							</CardText>
 						</CardBody>
@@ -191,13 +100,11 @@ class CardExample extends Component {
 							</a>
 						</CardFooter>
 					</Card>
-				</CardDeck>
-				<CardDeck className=" alex lead wow fadeInUp" data-wow-delay="0.5s" >
 					<Card>
-						<CardImg className="photo" top width="100%" src={alex} alt="Alex Lee" />
+						<CardImg top width="100%" src={matt} alt="Matthew Tejada" />
 						<CardBody>
-							<CardTitle className="cardTitle">Alex Lee</CardTitle>
-							<CardSubtitle className="subtitle">Card subtitle</CardSubtitle>
+							<CardTitle className="cardTitle">Matthew Tejada</CardTitle>
+							<CardSubtitle className="subtitle">CFO</CardSubtitle>
 							<CardText className="text">
 								This is a wider card with supporting text below as a natural lead-in to additional
 								content. This card has even longer content than the first to show that equal height
@@ -205,8 +112,109 @@ class CardExample extends Component {
 							</CardText>
 						</CardBody>
 						<CardFooter>
+							<a href="http://linkedin.com/in/charity-waddy-b48668112">
+								<FontAwesomeIcon ClassName="icon" icon={[ 'fab', 'linkedin' ]} />
+							</a>
+						</CardFooter>
+					</Card>
+				</CardDeck>
+					
+				<CardDeck className="lead wow fadeInUp" data-wow-delay={CARD_DELAY}>
+					<Card>
+						<CardImg className="photo" top width="100%" src={ahmad} alt="Ahmad Kalifeh" />
+						<CardBody>
+							<CardTitle className="cardTitle">Ahmad Kalifeh</CardTitle>
+							<CardSubtitle className="subtitle">Card subtitle</CardSubtitle>
+							<CardText className="text">
+								This card has supporting text below as a natural lead-in to additional content.
+							</CardText>
+						</CardBody>
+						<CardFooter>
+							<a href="https://www.linkedin.com/in/ben-davis-a34a55149/">
+								<FontAwesomeIcon ClassName="icon" icon={[ 'fab', 'linkedin' ]} />
+							</a>
+							<a href="https://github.com/bdavs3">
+								<FontAwesomeIcon ClassName="icon" icon={[ 'fab', 'github' ]} />
+							</a>
+							<a href="http://google.com">
+								<FontAwesomeIcon ClassName="icon" icon={[ 'fas', 'user-circle' ]} />
+							</a>
+						</CardFooter>
+					</Card>
+					<Card>
+						<CardImg top width="100%" src={alex} alt="Alex Lee" />
+						<CardBody>
+							<CardTitle>Alex Lee</CardTitle>
+							<CardSubtitle>Electrical Engineering</CardSubtitle>
+							<CardText>
+								Alex is both a talented electrical engineer and athlete.  At Mobiedock, he is working on electrical hardware components for the stations.  He is also on a team at Loyola Marymount that is implementing an innovative space technology within the Earth-Moon-Earth communication project.  When he's not tinkering with circuit boards and modeling software, Alex competes at a national level in kayaking with Team USA and is training for the 2020 Olympics.  
+							</CardText>
+						</CardBody>
+						<CardFooter>
 							<a href="http://google.com">
 								<FontAwesomeIcon ClassName="icon" icon={[ 'fab', 'linkedin' ]} />
+							</a>
+						</CardFooter>
+					</Card>
+					<Card>
+						<CardImg top width="100%" src={andre} alt="Andre De Leon" />
+						<CardBody>
+							<CardTitle>Andre De Leon</CardTitle>
+							<CardSubtitle>Mechanical Engineering</CardSubtitle>
+							<CardText>
+								This is a wider card with supporting text below as a natural lead-in to additional
+								content. This content is a little bit longer.
+							</CardText>
+						</CardBody>
+						<CardFooter>
+							<a href="http://google.com">
+								<FontAwesomeIcon ClassName="icon" icon={[ 'fab', 'linkedin' ]} />
+							</a>
+						</CardFooter>
+					</Card>
+				</CardDeck>
+				
+				<CardDeck className="lead wow fadeInUp" data-wow-delay={CARD_DELAY}>
+					<Card>
+						<CardImg top width="100%" src={emily} alt="Emily Shoji" />
+						<CardBody>
+							<CardTitle>Emily Shoji</CardTitle>
+							<CardSubtitle>Frontend Developer</CardSubtitle>
+							<CardText>
+								This is a wider card with supporting text below as a natural lead-in to additional
+								content. This content is a little bit longer.
+							</CardText>
+						</CardBody>
+						<CardFooter>
+							<a href="https://www.linkedin.com/in/emily-shoji/">
+								<FontAwesomeIcon ClassName="icon" icon={[ 'fab', 'linkedin' ]} />
+							</a>
+							<a href="https://github.com/ItsEmShoji">
+								<FontAwesomeIcon ClassName="icon" icon={[ 'fab', 'github' ]} />
+							</a>
+							<a href="https://itsemshoji.github.io/">
+								<FontAwesomeIcon ClassName="icon" icon={[ 'fas', 'user-circle' ]} />
+							</a>
+						</CardFooter>
+					</Card>
+					<Card>
+						<CardImg top width="100%" src={nico} alt="Danico Pidlaoan" />
+						<CardBody>
+							<CardTitle>Danico Pidlaoan</CardTitle>
+							<CardSubtitle>Project Manager / Frontend Developer</CardSubtitle>
+							<CardText>
+								This card has supporting text below as a natural lead-in to additional content.
+							</CardText>
+						</CardBody>
+						<CardFooter>
+							<a href="https://www.linkedin.com/in/danico-pidlaoan/">
+								<FontAwesomeIcon ClassName="icon" icon={[ 'fab', 'linkedin' ]} />
+							</a>
+							<a href="https://github.com/danicobryan">
+								<FontAwesomeIcon ClassName="icon" icon={[ 'fab', 'github' ]} />
+							</a>
+							<a href="https://danicobryan.github.io/">
+								<FontAwesomeIcon ClassName="icon" icon={[ 'fas', 'user-circle' ]} />
 							</a>
 						</CardFooter>
 					</Card>
